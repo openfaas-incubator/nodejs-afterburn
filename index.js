22de03f24844b4c17b0ae2725bfde56203f299fd
Alex Ellis <alexellis2@gmail.com>
2017-10-03 17:15:44 +0100

    multi-parse attempt
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- index.js
+++ index.js
@@ -3,22 +3,38 @@ var stdin = process.openStdin();
 var fs = require('fs');
 var handler = require('./handler');
 
+var header = null;
 
-stdin.addListener("data", function(d) {
+stdin.addListener("data", function(data) {
+    received = Buffer.concat([received, data]);
+
     let key = "\r\n\r\n";
     let keyBuffer = new Buffer(key);
-    let index = d.indexOf(key);
+    let index = received.indexOf(key);
 
-    let st = d.toString();
-    // console.log(st.substring(0, st.indexOf(key)))
+    let headerLength = index + keyBuffer.byteLength; // received.length - (index + keyBuffer.byteLength);
 
-    let body = new Buffer(d.length - (index+keyBuffer.byteLength));
-    d.copy(body, 0, index+keyBuffer.byteLength);
-    // console.log(body.toString())
+    let header = new Buffer(headerLength);
+    received.copy(header, 0, 0, headerLength);
 
+    let parsedHeader = parseHeader(header.toString());
+    let bodyLength = parsedHeader["Content-Length"];
+
+    let body = new Buffer(parseInt(bodyLength));
+    if(bodyLength + headerLength < received.length) {
+        console.log(received.length ,headerLength , bodyLength)
+        process.exit(1)
+    }
+    received.copy(body, index , 0, bodyLength);
+
+
+    // clear down buffer.
+    received = new Buffer(0);
+    
     handler(body.toString(), (err, output) => {
         let result = addHttp(output);
-        let done = process.stdout.write(result);
+        process.stdout.write(result);
+
         // process.exit();
     });
 });
@@ -29,3 +45,16 @@ function addHttp(content) {
     "\r\n" + 
     content);
 }
+
+function parseHeader(raw) {
+    let map = {};
+
+    raw.split("\r\n").reduce((m,obj) => {
+        if(obj.length) {
+            map[obj.substring(0, obj.indexOf(" ")-1 )] = obj.substring(obj.indexOf(" ")+1)
+        }
+    });
+    return map;
+}
+
+let received = new Buffer(0);
